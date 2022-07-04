b080650ec8dfcac88468e087866e785e41325064
Oktay Şenkan <oktay.senkan@gmail.com>
2022-07-04 03:16:10 +0300

    add oh my zsh install script

---
+++ scripts/setup_oh_my_zsh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh already installed!"
+fi
